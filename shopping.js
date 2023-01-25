cfd1988d93db0c3d792e70470742a5ae43f45bbf
Mounika <mounikagurram274@gmail.com>
2023-01-25 19:25:13 +0530

    first commit

---
+++ shopping.js
@@ -0,0 +1,57 @@
+const btn1 = document.getElementById('color-btn1');
+const btn2 = document.getElementById('color-btn2');
+const btn3 = document.getElementById('color-btn3');
+const btn4 = document.getElementById('color-btn4');
+
+const nav = document.getElementById('nav');
+const ul = document.getElementById('ul');
+const shoppingBtn = document.getElementById('shopping-btn')
+const sectionColor = document.getElementById('section-container');
+const shopbtn = document.getElementById('shopping-btn');
+const container2 = document.getElementById('container2');
+const container3 = document.getElementById('container3');
+const contactContainer = document.getElementById('contact-container')
+
+
+
+
+btn1.onclick = () => {
+    sectionColor.style.backgroundColor = '#FFC900';
+    nav.style.backgroundColor = '#FFC900';
+    ul.style.backgroundColor = '#FFC900';
+    shoppingBtn.style.backgroundColor = '#FFC900';
+    container2.style.backgroundColor = '#FFC900';
+    container3.style.backgroundColor = '#FFC900';
+    contactContainer.style.backgroundColor = '#FFC900';
+
+}
+btn2.onclick = () => {
+    sectionColor.style.backgroundColor = '#84DFFF';
+    nav.style.backgroundColor = '#84DFFF';
+    ul.style.backgroundColor = '#84DFFF';
+    shoppingBtn.style.backgroundColor = '#84DFFF';
+    container2.style.backgroundColor = '#84DFFF';
+    container3.style.backgroundColor = '#84DFFF';
+    contactContainer.style.backgroundColor = '#84DFFF';
+}
+
+btn3.onclick = () => {
+    sectionColor.style.backgroundColor = '#95CD41';
+    nav.style.backgroundColor = '#95CD41';
+    ul.style.backgroundColor = '#95CD41';
+    shoppingBtn.style.backgroundColor = '#95CD41';
+    container2.style.backgroundColor = '#95CD41';
+    container3.style.backgroundColor = '#95CD41';
+    contactContainer.style.backgroundColor = '#95CD41';
+
+}
+
+btn4.onclick = () => {
+    sectionColor.style.backgroundColor = '#ff5f7e';
+    nav.style.backgroundColor = '#ff5f7e';
+    ul.style.backgroundColor = '#ff5f7e';
+    shoppingBtn.style.backgroundColor = '#ff5f7e';
+    container2.style.backgroundColor = '#ff5f7e';
+    container3.style.backgroundColor = '#ff5f7e';
+    contactContainer.style.backgroundColor = '#ff5f7e';
+}
